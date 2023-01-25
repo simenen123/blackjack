67e82fc04d10075bbe17ae64626aac85df399247
simenen123 <Simenlog@outlook.com>
2023-01-25 15:21:00 +0100

    sdfs

--- blackjack.js
+++ blackjack.js
@@ -1,11 +1,14 @@
 let user_points = 0
 
+
 users_cards = []
 
 kort = [1,1,1,1, 2,2,2,2, 3,3,3,3, 4,4,4,4,]
 
 let length =  kort.length;
+     
 
+console.log("hello", user_points)
 
 let i = 0
 while (i < 2) {
@@ -16,3 +19,11 @@ while (i < 2) {
     i++;
   }
   console.log(users_cards)
+
+  
+// Running the for loop
+for (let i = 0; i < users_cards.length; i++) {
+    user_points += users_cards[i];
+}
+  
+console.log("Sum is " + user_points) // Prints: 44
