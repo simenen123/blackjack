2aa666729023bd0b4420d96a52a3b4990909918d
simenen123 <Simenlog@outlook.com>
2023-01-25 15:29:35 +0100

    sdfsdf

--- blackjack.js
+++ blackjack.js
@@ -14,16 +14,13 @@ let i = 0
 while (i < 2) {
     random_card_index = Math.floor(Math.random() * length);
     vårt_kort = kort[random_card_index]
-    console.log(vårt_kort)
     users_cards.push(vårt_kort)
     i++;
   }
-  console.log(users_cards)
 
   
 // Running the for loop
 for (let i = 0; i < users_cards.length; i++) {
     user_points += users_cards[i];
 }
-  
-console.log("Sum is " + user_points) // Prints: 44
+kort.splice(2,1)
