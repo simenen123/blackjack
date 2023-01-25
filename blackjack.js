da59b72724ce94dfaee07872fea91de266386ca7
simenen123 <Simenlog@outlook.com>
2023-01-25 14:07:45 +0100

    fsdfs

---
+++ blackjack.js
@@ -0,0 +1,20 @@
+let user_points = 0
+
+users_cards = []
+
+kort = [1,1,1,1, 2,2,2,2, 3,3,3,3, 4,4,4,4,]
+
+function start_game(){
+
+}
+
+
+
+i = 0
+
+while (i < 2);{
+    random_card = math.floor(Math.random() * kort.lenght);
+    console.log(random_card)
+    let removed_item = kort.indexOf()
+    i++
+}
